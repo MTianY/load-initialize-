2e3aa786d8a80424ab2203bd1f27abcb4f25db20
MTianY <mtystar@qq.com>
2018-08-22 22:46:21 +0800

    init

---
+++ load/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  load
+//
+//  Created by 马天野 on 2018/8/22.
+//  Copyright © 2018年 Maty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
